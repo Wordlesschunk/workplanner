30145a570405c5676ad64bc248149da83db7ef30
Dylan Jennings <dylan.jennings077@gmail.com>
2025-09-16 20:22:38 +0100

    recurring events

--- src/EventSubscriber/CalendarSubscriber.php
+++ src/EventSubscriber/CalendarSubscriber.php
@@ -29,50 +29,27 @@ class CalendarSubscriber implements EventSubscriberInterface
         $end = $setDataEvent->getEnd();
         $filters = $setDataEvent->getFilters();
 
-        $calendarEvent = $this->entityManager->getRepository(CalendarEvent::class)->findAll();
+        $setDataEvent->addEvent(new Event(
+            'Event 1',
+            new \DateTime('today 5am'),
+            new \DateTime('today 9am')
+        ));
 
-        dump($calendarEvent);
+        $fcEvent = new Event(
+            'FooBar',
+            new \DateTime('today 11am'),
+            new \DateTime('today 1pm'),
+        );
 
-        foreach ($calendarEvent as $event) {
+        $fcEvent->addOption('rrule', [
+            'freq' => 'weekly',
+            'interval' => 1,
+            'byweekday' => ['MO', 'WE'],
+            'dtstart' => '2025-09-01T10:00:00Z',
+//            'until'   => $event->getEndTime()->modify('+2 months')->format('Y-m-d\TH:i:s'),
+        ]);
 
-            $setDataEvent->addEvent(new Event(
-                $event->getTitle(),
-                new \DateTime($event->getStartTime()->format('Y-m-d h:i:s A')),
-                new \DateTime($event->getEndTime()->format('Y-m-d h:i:s A'))
-            ));
-        }
-
-
-        $tasks = [
-            ['title' => 'Task 1', 'duration' => 7200], // 2 hours
-            ['title' => 'Task 2', 'duration' => 19800], // 5.5 hour
-            ['title' => 'Task 4', 'duration' => 7200], // 5.5 hour
-            ['title' => 'Task 6', 'duration' => 30240], // 5.5 hour
-            ['title' => 'Task 3', 'duration' => 19800], // 5.5 hour
-        ];
-
-//        $calendarEvents = $this->entityManager->getRepository(CalendarEvent::class)->findBy(['title' => 'LUNCH']);
-        $calendarEvents = $this->entityManager->getRepository(CalendarEvent::class)->findAll();
-
-        $scheduler = new SchedulerService();
-        $scheduler->setBreakMinutes(15);
-        $scheduler->setCalendarEvents($calendarEvents);
-
-        $scheduled = $scheduler->scheduleTasks($tasks);
-
-        foreach ($scheduled as $event) {
-
-            $setDataEvent->addEvent(new Event(
-                $event->getTitle(),
-                new \DateTime($event->getStartTime()->format('Y-m-d h:i:s A')),
-                new \DateTime($event->getEndTime()->format('Y-m-d h:i:s A'))
-            ));
-
-//            echo $event->getTitle() . ' - ' . $event->getStartTime()->format('Y-m-d H:i')
-//                . ' to ' . $event->getEndTime()->format('H:i') . PHP_EOL;
-        }
-
-
+        $setDataEvent->addEvent($fcEvent);
 
     }
 }
